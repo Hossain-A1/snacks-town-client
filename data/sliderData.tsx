22f033935430beafd7c766283b827439c012b052
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-05-30 18:53:44 +0300

    slide completed

--- data/sliderData.tsx
+++ data/sliderData.tsx
@@ -8,19 +8,19 @@ export const data = [
     cta:"Get now"
   },
   {
-    id:1,
-    src:"https://images.pexels.com/photos/5908255/pexels-photo-5908255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt:"onion pizza",
-    title:"A Pepperoni Pizza for children",
-    para:"Pepperoni pizza is an American pizza variety which includes one of the country's most beloved toppings",
-    cta:"Get now"
+    id:2,
+    src:"https://images.pexels.com/photos/6287534/pexels-photo-6287534.jpeg?auto=compress&cs=tinysrgb&w=600",
+    alt:"pasta",
+    title:"Delicious dry pasta",
+    para:"Good pasta will have a chewy texture that has a bounce to it when chewed. These are qualities that you cannot determine until the pasta has been cooked but if the pasta wants to stick in a clump after being cooked or has too soft a texture when chewed",
+    cta:"Order now"
   },
   {
-    id:1,
-    src:"https://images.pexels.com/photos/5908255/pexels-photo-5908255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt:"onion pizza",
-    title:"A Pepperoni Pizza for children",
-    para:"Pepperoni pizza is an American pizza variety which includes one of the country's most beloved toppings",
-    cta:"Get now"
+    id:3,
+    src:"https://images.pexels.com/photos/552056/pexels-photo-552056.jpeg?auto=compress&cs=tinysrgb&w=600",
+    alt:"testy burger",
+    title:"Hangry Burger",
+    para:"burger is a flat round mass of minced meat or vegetables, which is fried and often eaten in a bread roll.",
+    cta:"view items"
   },
 ]
